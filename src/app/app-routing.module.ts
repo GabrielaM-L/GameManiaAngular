5429b07ed9360f654dade17a51b94b1aa70cd0e9
gabym <gabriela.marcominilima@gmail.com>
2022-01-17 09:44:39 -0300

    Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './views/clientes/clientes.component';
+import { ContatosComponent } from './views/contatos/contatos.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ParceirosComponent } from './views/parceiros/parceiros.component';
+import { SobreComponent } from './views/sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "sobre", component: SobreComponent},
+  { path: "clientes", component: ClientesComponent},
+  { path: "parceiros", component: ParceirosComponent},
+  { path: "contatos", component: ContatosComponent},
+  { path: "login", component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { SobreComponent } from './views/sobre/sobre.component';
+import { HeaderComponent } from './views/header/header.component';
+import { ClientesComponent } from './views/clientes/clientes.component';
+import { ParceirosComponent } from './views/parceiros/parceiros.component';
+import { ContatosComponent } from './views/contatos/contatos.component';
+import { FooterComponent } from './views/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './views/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    HeaderComponent,
+    ClientesComponent,
+    ParceirosComponent,
+    ContatosComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
